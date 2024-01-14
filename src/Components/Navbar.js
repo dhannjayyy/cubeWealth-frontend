0c61a87b1d01f0f520e09cd446be4b028173b678
Dhananjay <144516937+dhnnjayyy@users.noreply.github.com>
2024-01-14 22:50:01 +0530

    final changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,20 @@
 import React from "react";
+import { getUser } from "../helper/userSlice";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const user = useSelector(getUser)
   return (
     <nav className="bg-theme-black-700 text-white max-w-[1440px] mx-auto p-4">
       <ul className="flex justify-between items-center">
         <li>
-          {/* <img
-            src="https://assets-global.website-files.com/5f3f94d9ae99fbcd5528cc1f/5f3f94d9ae99fb490b28cccb_image%201.png"
-            className="w-[40%]"
-            alt="cube wealth icon"
-          /> */}
           <h2 className="text-2xl font-bold uppercase">
             Tasks
             <br /> Manager
           </h2>
+        </li>
+        <li>
+          <span>{user?.email}</span>
         </li>
       </ul>
     </nav>
